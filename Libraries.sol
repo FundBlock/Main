ce77b13641abe1276cc73a112745b4e187a26937
FundBlock <38694616+FundBlock@users.noreply.github.com>
2018-05-09 23:51:21 +0200

    Create Libraries.sol

---
+++ Libraries.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.23;
+
+library PoolLibrary {
+    enum State {Active, Cancelled, PaidOut, Finished}
+}
+
+library ParticipantsLibrary {
+    using SafeMath for uint256;
+    struct participant {
+        uint256 balance;
+        uint256 claimedTokens;
+    }
+}
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
